a308c243ea47fed38a6576e3d9467c1d92f04226
ksomao <somao@outlook.com>
2017-11-13 19:01:18 +0100

    db and validation added

---
+++ Model/Db.php
@@ -0,0 +1,69 @@
+<?php
+
+class Db
+{
+
+    public $db;
+
+    public function __construct()
+    {
+        try {
+            $this->db = new PDO('mysql:host=localhost;dbname=chatbox', 'root', '');
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            die('Erreur :' . $e->getMessage());
+        }
+    }
+
+    public function signUp($pseudo, $password, $sexe)
+    {
+        $req = $this->db->prepare("INSERT INTO Users (pseudo,password,sexe)VALUES  (?,?,?)");
+        $req->execute([$pseudo, $password, $sexe]);
+
+    }
+
+    public function login($pseudo, $password)
+    {
+        $req = $this->db->prepare("SELECT * FROM Users where pseudo = (?)");
+        $req->execute([$pseudo]);
+        if ($req->rowCount() == 1) {
+            $user = $req->fetchObject();
+
+            if ($user->password == $password) {
+                session_start();
+                $_SESSION["user"] = $user;
+                $_SESSION["logged"] = true;
+                echo "you are logged in";
+            }
+        }
+    }
+
+    public function logout()
+    {
+        $_SESSION["logged"] = false;
+        session_start();
+        session_unset();
+        session_destroy();
+        header("location:../index.php");
+    }
+
+    public function newMessage($text, $user_id)
+    {
+        $req = $this->db->prepare("INSERT INTO Messages (text,id_user)VALUES  (?,?)");
+        $req->execute([$text, $user_id]);
+    }
+
+    public function getMessages()
+    {
+        $messages = $this->db->query("SELECT * FROM Users,Messages Where Users.id = Messages.id_user ORDER BY TIME DESC ");
+        $messageArray = [];
+        while ($message = $messages->fetchObject()) {
+            $messageArray[] = $message;
+        }
+
+        return $messageArray;
+    }
+}
+
+?>
+
