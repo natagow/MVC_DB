0ad5d23523644127bc6c883ccf030cea2274bcd2
Belpois Nicolas <nicolasbelpois@gmail.com>
2017-11-13 10:54:24 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Views/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./Assets/CSS/login.css">
+    <title>Login</title>
+  </head>
+  <body>
+    <div class="header"></div>
+
+    <div class="main">
+
+      <div class="form-style-6">
+        <h1>Login</h1>
+        <form>
+        <input type="text" name="field1" placeholder="Your Name" />
+        <input type="text" name="field2" placeholder="Password" />
+        <input type="submit" value="Send" />
+        </form>
+      </div>
+
+    </div>
+
+
+  </body>
+</html>
